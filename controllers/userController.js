f50e37e046ae500fa8bb84d8b994b0eea8a6dfcf
MD Shimul <mdshemul480@gmail.com>
2021-06-28 18:07:12 +0600

    checking if user already exist or not

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
 import { validationResult } from "express-validator"
 
-export const register = (req, res) => {
+import UserModel from "../models/UserModel.js"
+
+
+export const register = async (req, res) => {
     const { name, email, password } = req.body
     const validationError = validationResult(req)
 
     if (!validationError.isEmpty()) {
         return res.status(401).json(validationError.array())
     }
+
+
+    try {
+        const checkUser = await UserModel.findOne({ email })
+        if (checkUser) {
+            return res.status(403).json({ error: [{ msg: "Email is already taken." }] })
+        }
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+
 }
