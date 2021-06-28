40b1fa134f7d42ddcef2955382353ccf9244caee
MD Shimul <mdshemul480@gmail.com>
2021-06-29 00:47:01 +0600

    adding home and moving login and signup page to the auth folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        
+      </Switch>
+    </Router>
   );
 }
 
