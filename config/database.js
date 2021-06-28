0c098a4e9925b0ee0e58f967fbf9e31a93a86aed
MD Shimul <mdshemul480@gmail.com>
2021-06-28 16:55:58 +0600

    removing unused variable

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connect = async () => {
     try {
-        const response = await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("DB connected.")
     } catch (err) {
         console.log(err)
