0ed29d3712e77fc2be1d0092e6011e73ba62c89e
MD Shimul <mdshemul480@gmail.com>
2021-06-28 18:04:37 +0600

    changing import in usermodel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
-import { model, Schema } from "mongoose"
+import mongoose from "mongoose"
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -17,4 +17,4 @@ const userSchema = new Schema({
 
 }, { timestamps: true })
 
-export default model("User", userSchema)
+export default mongoose.model("User", userSchema)
