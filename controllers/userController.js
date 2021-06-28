8b341edb8d924e5fbf761fd42d8ae2a87681339c
MD Shimul <mdshemul480@gmail.com>
2021-06-28 17:34:52 +0600

    throwing error for user input

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
-
+import { validationResult } from "express-validator"
 
 export const register = (req, res) => {
     const { name, email, password } = req.body
+    const validationError = validationResult(req)
 
+    if (!validationError.isEmpty()) {
+        return res.status(401).json(validationError.array())
+    }
 }
