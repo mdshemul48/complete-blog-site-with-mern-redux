6fc53f0a5636bc271cfd0727bc4ba88ea52c679d
MD Shimul <mdshemul480@gmail.com>
2021-06-29 00:51:11 +0600

    adding register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
+import Register from "./components/auth/Register";
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        
+        <Route path="/register" exact component={Register} />
       </Switch>
     </Router>
   );
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+    return (
+        <h1>register page</h1>
+    )
+}
+
+export default Register
