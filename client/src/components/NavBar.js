3b51d5be37050928405d52e1a7a314eca6f3053b
MD Shimul <mdshemul480@gmail.com>
2021-06-29 11:19:58 +0600

    adding right nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,6 +10,14 @@ const NavBar = () => {
                     </div>
                 </div>
             </div>
+            <div className="navbar__right">
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+            </div>
         </nav>
     )
 }
