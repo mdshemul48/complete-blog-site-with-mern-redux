885911c78bd301c224d61811ad12bbbc0879c8b9
MD Shimul <mdshemul480@gmail.com>
2021-06-29 12:51:40 +0600

    adding register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -9,7 +9,25 @@ const Register = () => {
                     <BgImage />
                 </div>
                 <div className="col-4">
+                    <div className="account">
+                        <div className="account__section">
+                            <form >
+                                <div className="group">
+                                    <input type="text" name="" className="group__control" placeholder="Enter Name" />
+                                </div>
+                                <div className="group">
+                                    <input type="email" name="" className="group__control" placeholder="Enter Email" />
+                                </div>
+                                <div className="group">
+                                    <input type="password" name="" className="group__control" placeholder="Create Password" />
+                                </div>
+                                <div className="group">
+                                    <input type="submit" className="btn btn-default btn-block" value="Register" />
 
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
