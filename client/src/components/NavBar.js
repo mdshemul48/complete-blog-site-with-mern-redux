d3231cd5ffd01f9530b7157a9ff12a15502f79f3
MD Shimul <mdshemul480@gmail.com>
2021-06-29 12:00:25 +0600

    fixing typo in nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,16 +8,17 @@ const NavBar = () => {
                     <div className="navbar__left">
                         <Link to="/"><img src="/images/logo.png" alt="" /></Link>
                     </div>
+                    <div className="navbar__right">
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </div>
                 </div>
             </div>
-            <div className="navbar__right">
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-            </div>
+
         </nav>
     )
 }
