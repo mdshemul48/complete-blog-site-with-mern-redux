e5dc0faf3cc3d32bfe1ef86ede64ba4ebeda405c
MD Shimul <mdshemul480@gmail.com>
2021-06-29 12:44:51 +0600

    creating saparated component for register bg

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
+import BgImage from './BgImage'
 const Register = () => {
     return (
         <>
             <div className="row mt-80">
                 <div className="col-8">
-                    <div className="bgImage">
-                        
-                    </div>
+                    <BgImage />
                 </div>
                 <div className="col-4">
 
