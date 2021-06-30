e4747452274f598b49c4ed7d9233ebf56c8fb823
MD Shimul <mdshemul480@gmail.com>
2021-06-30 14:04:19 +0600

    adding input and submit handler

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Helmet } from "react-helmet"
 import BgImage from './BgImage'
 const Register = () => {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const inputHandler = event => {
+        setFormState((prevState) => {
+            return { ...prevState, [event.target.name]: event.target.value }
+        })
+    }
+
+    const registerSubmitHandler = (event) => {
+        event.preventDefault()
+
+        console.log(formState)
+
+
+    }
+
     return (
         <>
             <Helmet>
@@ -15,18 +35,18 @@ const Register = () => {
                 <div className="col-4">
                     <div className="account">
                         <div className="account__section">
-                            <form >
+                            <form onSubmit={registerSubmitHandler}>
                                 <div className="group">
                                     <h3 className="form-heading">Register</h3>
                                 </div>
                                 <div className="group">
-                                    <input type="text" name="" className="group__control" placeholder="Enter Name" />
+                                    <input type="text" name="name" value={formState.name} className="group__control" placeholder="Enter Name" onChange={inputHandler} />
                                 </div>
                                 <div className="group">
-                                    <input type="email" name="" className="group__control" placeholder="Enter Email" />
+                                    <input type="email" name="email" value={formState.email} className="group__control" placeholder="Enter Email" onChange={inputHandler} />
                                 </div>
                                 <div className="group">
-                                    <input type="password" name="" className="group__control" placeholder="Create Password" />
+                                    <input type="password" name="password" value={formState.password} className="group__control" placeholder="Create Password" onChange={inputHandler} />
                                 </div>
                                 <div className="group">
                                     <input type="submit" className="btn btn-default btn-block" value="Register" />
