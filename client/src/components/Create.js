d2de1aa66f5f265c16cc1f095eeb2e45ed9f488a
MD Shimul <mdshemul480@gmail.com>
2021-07-18 14:22:19 +0600

    adding margin and padding to the post form and adding the post url

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -27,33 +27,44 @@ const Create = () => {
       <meta name="description" content="create a new post" />
     </Helmet>
     <div className="container">
-      <div className="row">
-        <div className="col-6">
-          <div className="card">
-            <h3 className="card__h3">Create a new post</h3>
-            <form>
-              <div className="group">
-                <label htmlFor="title">
-                  Post Title
-                </label>
-                <input type="text" name="title" id="title" value={formState.title} onChange={inputHandler} className="group__control" placeholder="Post Title..." />
+      <form>
+        <div className="row ml-minus-15 mr-minus-15">
+          <div className="col-6 p-15">
+            <div className="card">
+              <h3 className="card__h3">Create a new post</h3>
+              <form>
+                <div className="group">
+                  <label htmlFor="title">
+                    Post Title
+                  </label>
+                  <input type="text" name="title" id="title" value={formState.title} onChange={inputHandler} className="group__control" placeholder="Post Title..." />
+                </div>
+                <div className="group">
+                  <label htmlFor="image" className="image__label">{currentImage}</label>
+                  <input type="file" name="picture" id="image" onChange={fileHandler} />
+                </div>
+                <div className="group">
+                  <label htmlFor="body">
+                    Post Body
+                  </label>
+                  <ReactQuill id="body" theme="snow" value={blogPost} onChange={setBlogPost} />
+                </div>
+                <div className="group">
+                  <input type="submit" value="Create post" className="btn btn-default btn-block" />
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-6 p-15">
+            <div className="card">
+              <div className="group ">
+                <label htmlFor="slug">Post URL</label>
+                <input type="text" name="slug" id="slug" className="group__control" placeholder="post URL..." />
               </div>
-              <div className="group">
-                <label htmlFor="image" className="image__label">{currentImage}</label>
-                <input type="file" name="picture" id="image" onChange={fileHandler} />
-              </div>
-              <div className="group">
-                <label htmlFor="body">
-                  <ReactQuill theme="snow" value={blogPost} onChange={setBlogPost} />
-                </label>
-              </div>
-              <div className="group">
-                <input type="submit" value="Create post" className="btn btn-default btn-block" />
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>;
 };
