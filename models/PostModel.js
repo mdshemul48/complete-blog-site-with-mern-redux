2d32b28d74d30dde87054e7d854ff9b24c600b82
MD Shimul <mdshemul480@gmail.com>
2021-07-22 19:25:08 +0600

    adding post schema

--- models/PostModel.js
+++ models/PostModel.js
@@ -1,3 +1,35 @@
 import mongoose from "mongoose"
 
-const 
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+
+    }
+}, { timestamps: true })
+
+export default mongoose.model("Post", postSchema)
