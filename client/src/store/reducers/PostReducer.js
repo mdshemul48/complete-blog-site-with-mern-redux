7907d0462456c34501e1577a221362a809aca630
MD Shimul <mdshemul480@gmail.com>
2021-07-22 17:47:49 +0600

    creating reducer for create post

--- client/src/store/reducers/PostReducer.js
+++ client/src/store/reducers/PostReducer.js
@@ -1,10 +1,21 @@
+import { SET_LOADER, CLOSE_LOADER, CREATE_ERRORS } from "../types/PostTypes"
+
 const initState = {
     loading: false,
     createErrors: []
 }
 
 const PostReducer = (state = initState, action) => {
-    return state
+    const { type, payload } = action
+    if (action.type === SET_LOADER) {
+        return { ...state, loading: true }
+    } else if (type === CLOSE_LOADER) {
+        return { ...state, loading: false }
+    } else if (type === CREATE_ERRORS) {
+        return { ...state, createErrors: payload }
+    } else {
+        return state
+    }
 }
 
 export default PostReducer
