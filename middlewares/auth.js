7cee9ce3aed92ad0f68d51634be2a9f6442ff395
MD Shimul <mdshemul480@gmail.com>
2021-07-23 15:59:20 +0600

    adding auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,12 @@
+import jwt from "jsonwebtoken"
+
 
 export default (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1]
-    console.log(token);
-    next()
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+        jwt.verify(token, process.env.SECRET)
+        next()
+    } catch (err) {
+        return res.status(401).json({ errors: [{ msg: err.message }] })
+    }
 }
