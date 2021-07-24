533f6d4fffea7e949287ba48b274dd23c7c9d4ee
MD Shimul <mdshemul480@gmail.com>
2021-07-24 17:15:11 +0600

    adding post to dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { REDIRECT_FALSE, REMOVE_MESSAGE } from '../store/types/PostTypes'
 import { Toaster, toast } from "react-hot-toast"
 import { fetchPosts } from '../store/asyncMethods/PostMethods'
+import { Link } from 'react-router-dom'
 const Dashboard = () => {
     const { redirect, message, loading } = useSelector(state => state.PostReducer)
     const { user: { _id: id } } = useSelector(state => state.AuthReducer)
@@ -36,7 +37,25 @@ const Dashboard = () => {
 
                     }
                 }} />
-            <h1>Dashboard</h1>
+            <div className="container mt-100">
+                <div className="row">
+                    <div className="col-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam provident deleniti consectetur quos praesentium, mollitia soluta adipisci aperiam, harum saepe quo quam aut accusantium commodi debitis perspiciatis amet beatae sapiente.
+                    </div>
+                    <div className="col-9">
+                        {!loading ? posts.length > 0 ? posts.map(
+                            (post) => {
+                                return <div className="dashboard__post" key={post._id}>
+                                    <div className="dashboard_post__title">
+                                        <Link to="/">{post.title}</Link>
+                                    </div>
+                                </div>
+                            }
+                        ) : "you dont have any post" : "loading"}
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
