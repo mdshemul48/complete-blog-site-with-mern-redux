c08a0992525704ad033997af38f2e0d99b1bdc81
MD Shimul <mdshemul480@gmail.com>
2021-07-31 18:50:34 +0600

    adding middleware in post update routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,6 +6,7 @@ import {
   fetchPosts,
   fetchPost,
   updatePost,
+  updateValidations,
 } from "../controllers/postController.js";
 // middleware
 import auth from "../middlewares/auth.js";
@@ -13,7 +14,7 @@ import auth from "../middlewares/auth.js";
 const router = Router();
 
 router.post("/create_post", auth, createPost);
-router.put("/update", auth, updatePost);
+router.put("/update", [auth, updateValidations], updatePost);
 router.get("/posts/:id/:page", auth, fetchPosts);
 router.get("/post/:id", auth, fetchPost);
 
