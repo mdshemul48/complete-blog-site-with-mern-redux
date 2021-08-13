43a1b677fe4b7f56b307669b3c873f8b5001ceee
MD Shimul <mdshemul480@gmail.com>
2021-08-13 18:01:59 +0600

    adding validation in backend

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,1 +1,6 @@
-export const updateName = (req, res) => {};
+export const updateName = (req, res) => {
+  const { name, id } = req.body;
+  if (name === "") {
+    return res.status(400).json({ errors: [{ msg: "Name is required" }] });
+  }
+};
