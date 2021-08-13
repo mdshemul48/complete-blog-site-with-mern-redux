1985ef0c673504ce6cbb9e652caf67694f9a9b71
MD Shimul <mdshemul480@gmail.com>
2021-08-13 18:42:43 +0600

    changing username and creating new token

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,20 @@
-export const updateName = (req, res) => {
+import userModel from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import createToken from "../util/token.js";
+export const updateName = async (req, res) => {
   const { name, id } = req.body;
   if (name === "") {
     return res.status(400).json({ errors: [{ msg: "Name is required" }] });
+  } else {
+    try {
+      const user = await userModel.findOneAndUpdate(
+        { _id: id },
+        { name },
+        { new: true }
+      );
+      const token = createToken(user);
+    } catch (error) {
+      return res.status(500).json({ errors });
+    }
   }
 };
