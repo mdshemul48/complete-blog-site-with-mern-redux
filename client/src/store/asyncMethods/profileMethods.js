27b2d8fecc75aa92e5244bf609b2b8281d0077db
MD Shimul <mdshemul480@gmail.com>
2021-08-13 14:54:53 +0600

    adding profile method code

--- client/src/store/asyncMethods/profileMethods.js
+++ client/src/store/asyncMethods/profileMethods.js
@@ -1,3 +1,16 @@
-export const updateName = (req, res) => {
-  return (dispatch, getState) => {};
+import axios from "axios";
+
+export const updateNameAction = (req, res) => {
+  return (dispatch, getState) => {
+    const {
+      AuthReducer: { token },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+    } catch (error) {}
+  };
 };
