723e0ef49a86f352db7483d4fa06f6ee08f92988
MD Shimul <mdshemul480@gmail.com>
2021-08-14 19:52:28 +0600

    changing user password in backend

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,7 @@
+import { body, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 import userModel from "../models/UserModel.js";
 import createToken from "../util/token.js";
-import { body, validationResult } from "express-validator";
 import UserModel from "../models/UserModel.js";
 
 export const updateName = async (req, res) => {
@@ -28,18 +29,48 @@ export const updatePasswordValidation = [
     .isEmpty()
     .trim()
     .withMessage("Current password is required."),
+  body("userId").not().isEmpty().trim().withMessage("user id not found."),
   body("newPassword")
     .isLength({ min: 6 })
     .withMessage("New password must be 6 characters long."),
 ];
 
 export const updatePassword = async (req, res) => {
-  const { current, newPassword } = req.body;
+  const { currentPassword, newPassword, userId } = req.body;
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   } else {
-    const user = await UserModel.findOne();
+    try {
+      const user = await UserModel.findOne({ _id: userId });
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: "user not found." }] });
+      }
+
+      console.log(currentPassword, user.password);
+      const comparedPassword = await bcrypt.compare(
+        currentPassword,
+        user.password
+      );
+      if (!comparedPassword) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "current password is wrong" }] });
+      }
+
+      // hashing the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = await userModel.findOneAndUpdate(
+        { _id: userId },
+        { password: hashedPassword },
+        { new: true }
+      );
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ errors: [{ msg: error.message }] });
+    }
+    // const newpassqwo
   }
 };
