de42f6bcbde5987515e281c8df3dd9b1eced75fd
MD Shimul <mdshemul480@gmail.com>
2021-08-14 17:59:21 +0600

    connecting validator to password route

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,11 +3,16 @@ import auth from "../middlewares/auth.js";
 import {
   updateName,
   updatePassword,
+  updatePasswordValidation,
 } from "../controllers/profileController.js";
 
 const router = Router();
 
 router.post("/updateName", auth, updateName);
-router.post("/updatePassword", auth, updatePassword);
+router.post(
+  "/updatePassword",
+  [auth, updatePasswordValidation],
+  updatePassword
+);
 
 export default router;
