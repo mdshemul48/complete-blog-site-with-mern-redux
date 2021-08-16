65d9bee7482d4332b7d0ba871c395907819f37c2
MD Shimul <mdshemul480@gmail.com>
2021-08-16 20:05:28 +0600

    showing posts in home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { homePosts } from "../store/asyncMethods/PostMethods";
+import { Loader } from "./Loader";
 const Home = () => {
   let { page } = useParams();
 
@@ -22,6 +23,26 @@ const Home = () => {
         <title>Web articles</title>
         <meta name='description' content='Learn html, css, and javascript' />
       </Helmet>
+      <div className='container'>
+        <div className='col-8'>
+          {!loading ? (
+            posts.length > 0 ? (
+              posts.map((post) => {
+                return (
+                  <div className='row'>
+                    <div className='col-8'></div>
+                    <div className='col-4'>image section</div>
+                  </div>
+                );
+              })
+            ) : (
+              "No posts"
+            )
+          ) : (
+            <Loader />
+          )}
+        </div>
+      </div>
     </>
   );
 };
