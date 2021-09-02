cc515aa73d91809f56311210d73ac9647546d83a
MD Shimul <mdshemul480@gmail.com>
2021-09-02 17:05:38 +0600

    adding post details

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import dateformat from "dateformat";
+import htmlToText from "html2plaintext";
 import { postDetails } from "../store/asyncMethods/PostMethods";
 
 import Loader from "./Loader";
@@ -19,17 +20,25 @@ const Details = () => {
       <div className='row mt-100'>
         <div className='col-8'>
           {!loading && details?._id ? (
-            <div className='post_details'>
-              <div className='post__header'>
-                <div className='post__header__avator'>
-                  {details.userName[0]}
-                </div>
-                <div className='post__header__user'>
-                  <span>{details.userName}</span>
-                  <span>{dateformat(details.updatedAt)}</span>
+            <>
+              <div className='post_details'>
+                <div className='post__header'>
+                  <div className='post__header__avator'>
+                    {details.userName[0]}
+                  </div>
+                  <div className='post__header__user'>
+                    <span>{details.userName}</span>
+                    <span>{dateformat(details.updatedAt)}</span>
+                  </div>
                 </div>
               </div>
-            </div>
+              <div className='post__body'>
+                <h1 className='post__body__title'>{details.title}</h1>
+                <div className='post__body__details'>
+                  {htmlToText(details.body)}
+                </div>
+              </div>
+            </>
           ) : (
             <Loader />
           )}
