b9f98d0e047667738425ec2d36c907515350526f
MD Shimul <mdshemul480@gmail.com>
2021-09-02 15:29:07 +0600

    getting id from details url

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 const Details = () => {
+  const { id } = useParams();
   return (
     <div className='container'>
       <div className='row mt-100'>
