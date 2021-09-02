993801faafe0e1b2cc184f571b9c48ffadcd8afd
MD Shimul <mdshemul480@gmail.com>
2021-09-02 15:26:08 +0600

    adding details components for post

---
+++ client/src/components/Details.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+const Details = () => {
+  return (
+    <div className='container'>
+      <div className='row mt-100'>
+        <div className='col-8'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
