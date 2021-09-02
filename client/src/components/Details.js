30a844f3014c41e7aba8c384c0a1997d4abe391b
MD Shimul <mdshemul480@gmail.com>
2021-09-02 16:49:56 +0600

    adding dispather for fetching post from api

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { postDetails } from "../store/asyncMethods/PostMethods";
+
 const Details = () => {
   const { id } = useParams();
+  const { loading, details } = useSelector((state) => state.PostReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(postDetails(id));
+  }, [dispatch, id]);
+
   return (
     <div className='container'>
       <div className='row mt-100'>
